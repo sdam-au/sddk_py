c7d3989dbb6d7c4517618cc00d60912e6de9daed
Vojtech Kase <vojtech.kase@gmail.com>
2021-05-26 11:15:50 +0200

    distribution added

--- build/lib/sddk/__init__.py
+++ build/lib/sddk/__init__.py
@@ -20,7 +20,12 @@ from bs4 import BeautifulSoup
 
 
 def test_package():
-    print("here we are right now - May 11, 13:00")
+    print("here we are right now - May 26, 11:10")
+
+###
+# ORIGINAL VERSION OF THE PACKAGE, USING "conf"
+# search below for cloudSession() class, containing some simplifications
+####
 
 def configure_session_and_url(shared_folder_name=None, owner=None): ### insert group folder name or leave empty for personal root
     '''
@@ -110,6 +115,8 @@ def make_data_from_object(python_object, file_ending):
         return (type(python_object), open("temp.png", 'rb'))
     else:
         print("The function does not support " + str(type(python_object)) + " type of objects. Change the format of your data.")
+
+
 
 def gdf_to_geojson(gdf_input):
     # serialize geometry:
@@ -234,3 +241,159 @@ def list_filenames(directory="", filetype="", conf=None):
             if filetype in a_str:
                     filenames.append(a_str)
     return filenames
+
+class cloudSession:
+    def __init__(self, provider=None, shared_folder_name=None, owner=None): ### insert group folder name or leave empty for personal root
+        '''
+        interactively setup your sciencedata.dk homeurl, username and password
+        in the case of shared folder, inquire for its owner as well
+        check functionality and redirections
+        '''
+        if ("sciencedata.dk" in str(provider)) or (provider == None):
+                    ### set username and password
+            username = input("Your ScienceData username (e.g. '123456@au.dk'): ")
+            password = getpass.getpass("Your ScienceData password: ")
+            ### establish a request session
+            s = requests.Session()
+            s.auth = (username, password)
+            sciencedata_homeurl_alternatives = ["https://sciencedata.dk/","https://silo1.sciencedata.dk/","https://silo2.sciencedata.dk/","https://silo3.sciencedata.dk/","https://silo4.sciencedata.dk/"]
+            for sciencedata_homeurl_root in sciencedata_homeurl_alternatives:
+                if s.get(sciencedata_homeurl_root + "files/").ok:
+                    root_folder_url = sciencedata_homeurl_root + "files/"
+                    ### SETTING FOR SHARED FOLDER - if their name is passed in: 
+                    if shared_folder_name != None:
+                        shared_folder_owner_url = root_folder_url + shared_folder_name + "/"
+                        if s.get(shared_folder_owner_url).ok: ### if you are owner of the shared folder 
+                            root_folder_url = shared_folder_owner_url ### use the url as the endpoint
+                            print("connection with shared folder established with you as its owner")
+                        else: # otherwise use endpoint for "shared with me"
+                            if owner==None:
+                                owner = input("\"" + shared_folder_name + "\" owner's username: ") ### in the case Vojtech is folder owner
+                            shared_folder_member_url = sciencedata_homeurl_root + "sharingin/" + owner + "/" + shared_folder_name + "/" 
+                            try:
+                                redirection = s.get(shared_folder_member_url, allow_redirects=False).headers["Location"]
+                                if redirection != None:
+                                    shared_folder_member_url = redirection + "/"
+                            except:
+                                pass
+                            if s.get(shared_folder_member_url).ok:
+                                root_folder_url = shared_folder_member_url
+                                print("connection with shared folder established with you as its ordinary user")
+                            else:
+                                print("connection with shared folder failed")
+                break
+            print("endpoint variable has been configured to: " + root_folder_url)
+        if "owncloud.cesnet.cz" in provider:
+            user = input("Insert your Username code (a long string of characters and numbers): ")
+            password = getpass.getpass("Insert your Password/Token: ")
+            s = requests.Session() # create session
+            s.auth = (user, password)
+            root_folder_url = "https://owncloud.cesnet.cz/remote.php/dav/files/{0}/".format(user)
+            if s.get(root_folder_url).ok:
+                print("endpoint variable has been configured to: " + root_folder_url)
+                
+        self.s = s
+        self.root_folder_url = root_folder_url
+        
+    def check_path(self, path_and_filename):
+        while not self.s.get(self.root_folder_url + path_and_filename.rpartition("/")[0]).ok:
+            path_and_filename = input("The path is not valid. Try different path and filename: ")
+        if self.s.get(self.root_folder_url + path_and_filename.rpartition("/")[0]).ok:
+            return path_and_filename
+        else:
+            print("Sorry, it is still not okay.")
+
+    def check_filename(self, path_and_filename): 
+        '''
+        check whether there  exist a file with the same name
+        '''
+        if self.s.get(self.root_folder_url + path_and_filename).ok: ### if there already is a file with the same name
+            print("A file with the same name (\"" + path_and_filename.rpartition("/")[2] + "\") already exists in this location.")
+            approved_name = input("Press Enter to overwrite it or choose different path and filename: ")
+            if len(approved_name) == 0: 
+                approved_name = path_and_filename
+        else:
+            approved_name = path_and_filename           
+        return approved_name
+    
+    def write_file(self, path_and_filename, python_object):
+        '''
+        write file to specified location
+        '''
+        s = self.s
+        cloud_url = self.root_folder_url
+        path_and_filename = self.check_path(path_and_filename)
+        approved_name = self.check_filename(path_and_filename)
+        file_ending = approved_name.rpartition(".")[2]
+        data_processed = make_data_from_object(python_object, file_ending)
+        try:
+            if not approved_name.rpartition(".")[2] in ["txt", "json", "geojson", "csv", "png", "eps","feather"]:
+                new_filename_ending = input("Unsupported file format. Type either \"txt\", \"csv\", \"json\", \"geojson\", \"feather\", \"eps\", or \"png\": ")
+                approved_name = approved_name.rpartition(".")[0] + "." + new_filename_ending
+                approved_name = self.check_filename(approved_name) ### check whether the file exists for the second time (the ending changed)
+            s.put(cloud_url + approved_name, data=data_processed[1])
+            try:
+                os.remove("temp." + file_ending)
+            except:
+                pass
+            print("Your " + str(data_processed[0]) + " object has been succefully written as \"" + cloud_url + approved_name + "\"")
+        except:
+            print("Something went wrong. Check path, filename and object.")
+
+
+    def read_file(self, path_and_filename, object_type=None, public_folder=None):
+        s = self.s
+        cloud_url = self.root_folder_url
+        if "https" in path_and_filename:
+            cloud_url = ""
+        if object_type==None:
+            object_type="df"
+        if public_folder != None:
+            response = s.get("https://sciencedata.dk/public/" + public_folder + "/" + path_and_filename)
+        elif "public/" in path_and_filename:
+            response = s.get(path_and_filename)
+        else:
+            response = s.get(cloud_url + path_and_filename)
+        if response.ok:
+            try:
+                if object_type == "str":
+                    object_to_return = response.text
+                if object_type == "df":
+                    if ".csv" in path_and_filename:
+                        object_to_return = pd.read_csv(io.StringIO(response.text), index_col=0)
+                    elif ".json" in path_and_filename:
+                        object_to_return = pd.DataFrame(response.json())
+                    elif ".feather" in path_and_filename:
+                        object_to_return = pd.read_feather(io.BytesIO(response.content))
+                        for column in object_to_return.columns:
+                            try:
+                                object_to_return[column] = object_to_return[column].str.decode("utf-8")
+                            except:
+                                object_to_return[column] = object_to_return[column]
+                    else:
+                        object_to_return = pd.DataFrame(response.json())
+                if object_type == "gdf":
+                    object_to_return = gpd.read_file(io.BytesIO(response.content), driver='GeoJSON')
+                if object_type == "dict":
+                    object_to_return = json.loads(response.content)
+                if object_type == "list":
+                    object_to_return = json.loads(response.content)
+                return object_to_return
+            except:
+                print("file import failed")
+        else:
+            print("file has not been found; check filename and path.")
+
+    def list_filenames(self, directory="", filetype=""):
+        resp = self.s.get(self.root_folder_url + directory)
+        soup = BeautifulSoup(resp.content, "html.parser")
+        if "." not in filetype:
+            filetype = "." + filetype
+        filenames = []
+        for a in soup.find_all("a"):
+                a_str = str(a.get_text())
+                if filetype in a_str:
+                        filenames.append(a_str)
+        return filenames
+
+
